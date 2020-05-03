e882688f18968b08580f7a8463cdc5b1e0d32781
VikSala <vsz11697@gmail.com>
2020-05-03 18:45:03 +0200

    Nuevo proyecto 0

---
+++ Experimento/Intermediate/Build/Win64/UE4Editor/Inc/Experimento/ExperimentoGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EXPERIMENTO_ExperimentoGameModeBase_generated_h
+#error "ExperimentoGameModeBase.generated.h already included, missing '#pragma once' in ExperimentoGameModeBase.h"
+#endif
+#define EXPERIMENTO_ExperimentoGameModeBase_generated_h
+
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_SPARSE_DATA
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_RPC_WRAPPERS
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAExperimentoGameModeBase(); \
+	friend struct Z_Construct_UClass_AExperimentoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AExperimentoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Experimento"), NO_API) \
+	DECLARE_SERIALIZER(AExperimentoGameModeBase)
+
+
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAExperimentoGameModeBase(); \
+	friend struct Z_Construct_UClass_AExperimentoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AExperimentoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Experimento"), NO_API) \
+	DECLARE_SERIALIZER(AExperimentoGameModeBase)
+
+
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AExperimentoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AExperimentoGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AExperimentoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AExperimentoGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AExperimentoGameModeBase(AExperimentoGameModeBase&&); \
+	NO_API AExperimentoGameModeBase(const AExperimentoGameModeBase&); \
+public:
+
+
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AExperimentoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AExperimentoGameModeBase(AExperimentoGameModeBase&&); \
+	NO_API AExperimentoGameModeBase(const AExperimentoGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AExperimentoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AExperimentoGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AExperimentoGameModeBase)
+
+
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_12_PROLOG
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_SPARSE_DATA \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_RPC_WRAPPERS \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_INCLASS \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_SPARSE_DATA \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Experimento_Source_Experimento_ExperimentoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> EXPERIMENTO_API UClass* StaticClass<class AExperimentoGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Experimento_Source_Experimento_ExperimentoGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Experimento/Source/Experimento/ExperimentoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExperimentoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPERIMENTO_API AExperimentoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
